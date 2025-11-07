8e60fc0e96ead8b43751d63feffdbdb6559b4dd1
hellomraza <hellomraza@gmail.com>
2025-11-08 01:35:51 +0530

    Refactor HomePage component to use static random words from JSON instead of fetching, simplifying the code and improving performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Game from "@/components/Game";
-import { fetchRandomWords, generateText } from "@/utils/helper";
+import randomWords from "@/lib/words/random-words.json";
+import { generateText } from "@/utils/helper";
 
 import React from "react";
 
 const HomePage: React.FC = async () => {
-  const words = await fetchRandomWords();
+  const targetText = generateText(80, randomWords.words);
 
-  const targetText = generateText(80, words);
-
-  if (words.length === 0) {
+  if (targetText.length === 0) {
     return (
       <main className="min-h-screen flex flex-col items-center justify-center p-4">
         <div className="text-2xl font-semibold text-red-500">
